84428011b1d843e4aa0e8b1dd95f9801642d9f1b
arshad <jahan665577@gmail.com>
2023-07-12 09:39:17 +0500

    updated

--- lib/detail_screen.dart
+++ lib/detail_screen.dart
@@ -10,34 +10,30 @@ class DetailScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final counterBloc = context.read<CounterBloc>();
+    final counterBloc = BlocProvider.of<CounterBloc>(context, listen: true);
 
-    return BlocBuilder<CounterBloc, CounterState>(builder: (context, state) {
-      return Scaffold(
-          body: SafeArea(
-        child: ListView.builder(
-          itemCount: counterBloc.nameList.length,
-          itemBuilder: (context, index) {
-            if (state is StringState) {
-              return Card(
-                child: ListTile(
-                  leading: CircleAvatar(
-                    child: Text(state.nameList[index][0]),
-                  ),
-                  title: Text(state.nameList[index]),
-                  trailing: IconButton(
-                    onPressed: () {
-                      counterBloc
-                          .add(StringDeleteEvent(txt: state.nameList[index]));
-                    },
-                    icon: Icon(Icons.delete),
-                  ),
-                ),
-              );
-            }
-          },
-        ),
-      ));
-    });
+    return Scaffold(
+        body: SafeArea(
+      child: ListView.builder(
+        itemCount: counterBloc.nameList.length,
+        itemBuilder: (context, index) {
+          return Card(
+            child: ListTile(
+              leading: CircleAvatar(
+                child: Text(counterBloc.nameList[index][0]),
+              ),
+              title: Text(counterBloc.nameList[index]),
+              trailing: IconButton(
+                onPressed: () {
+                  counterBloc
+                      .add(StringDeleteEvent(txt: counterBloc.nameList[index]));
+                },
+                icon: Icon(Icons.delete),
+              ),
+            ),
+          );
+        },
+      ),
+    ));
   }
 }
